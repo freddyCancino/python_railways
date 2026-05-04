6cb195f4a48c3f6115bd2b961b9820e02e94498a
Alfredo Cancino <acancinom@hotmail.com>
2026-05-04 14:58:09 -0500

    config settings y migrations2 prod

--- contacto/apps.py
+++ contacto/apps.py
@@ -1,12 +1,23 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import User
 
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'correo@gmail.com', '12345678')
 
 
 class ContactoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contacto'
+
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                'admin',
+                'correo@gmail.com',
+                '12345678'
+            )
+
+
+
+
 
