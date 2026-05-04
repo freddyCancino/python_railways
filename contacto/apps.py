8e545764a913b3abd75d853332b9ec3315dae48c
Alfredo Cancino <acancinom@hotmail.com>
2026-05-04 14:44:41 -0500

    add user admin prod

--- contacto/apps.py
+++ contacto/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import User
+
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'correo@gmail.com', '12345678')
+
 
 class ContactoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contacto'
+
